c1c4612c56fbae800a47afd0bc15e6a72eb1813b
Do Ho Kim <33643752+Do-ho@users.noreply.github.com>
2020-11-19 19:49:11 +0900

    chore: Webpack, Babel, ESLint 설정 및 기본 폴더 구조 생성 (#5)
    * chore: gitignore 생성
    
    - vscode, react, node, dotenv에 대한 gitignore 생성
    
    * chore: 기본 프론트 서버 구조 세팅
    
    - atomic 디자인 패턴을 이용해 component 관리
    - 추후 index를 통한 폴더 관리를 위해 index.ts 추가
    
    * chore: favicon 추가
    
    * chore: 로고 png 제작
    
    * chore: dotenv 샘플 코드 작성
    
    * chore: 기본 프론트 package 추가
    
    - markdown 라이브러리 remarkable 설치
    - tslint를 사용하기 위한 라이브러리 설치
    - webpack을 사용하기 위한 라이브러리 설치
    
    * chore: prettier 세팅
    
    * chore: tsconfig 파일 작성
    
    - typescript를 컴파일하기 위한 tsconfig 작성
    
    * chore: gitignore env 설정 추가
    
    * chore: babel, lint 패키지 설치
    
    * chore: lint 설정 추가
    
    - 협업을 위한 lint 설정 추가
    
    * chore: babel, dotenv을 webpack에 적용
    
    - 브라우저 호환성을 위한 babel 설정
    - 환경변수로 node 모드를 다루기 위한 코드 변경
    
    * fix: Lint 적용에 따른 변경

---
+++ client/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, options) => {
+  dotenv.config({
+    path: `./env/${options.stage || 'development'}.env`
+  });
+
+  return {
+    mode: process.env.NODE_ENV,
+    entry: ['@babel/polyfill', './src/index.tsx'],
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      historyApiFallback: true,
+      inline: true,
+      port: 5000,
+      hot: true
+    },
+    output: {
+      path: `${__dirname}/dist`,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)?$/,
+          exclude: /node_modules/,
+          loader: 'awesome-typescript-loader',
+          options: {
+            useBabel: true,
+            babelCore: '@babel/core'
+          }
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', 'jsx', '.ts', '.tsx'],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@style': path.resolve(__dirname, 'src/common/style'),
+        '@utils': path.resolve(__dirname, 'src/common/utils'),
+        '@hook': path.resolve(__dirname, 'src/common/hook'),
+        '@context': path.resolve(__dirname, 'src/common/context'),
+        '@imgs': path.resolve(__dirname, 'public/imgs')
+      }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './public/index.html',
+        favicon: './public/favicon.ico'
+      }),
+      new CleanWebpackPlugin()
+    ]
+  };
+};
