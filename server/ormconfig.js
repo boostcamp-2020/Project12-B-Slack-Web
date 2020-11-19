0f0bfae51f512195781c509a4eada390cbc975e9
profornnan <profornnan@naver.com>
2020-11-20 04:04:06 +0900

    chore: typeORM 설정 추가
    - typeORM 관련 라이브러리 추가
    - typeORM 설정 파일 추가 (ormconfig.js)

---
+++ server/ormconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: ['src/model/**/*.ts']
+};
