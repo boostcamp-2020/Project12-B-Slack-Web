3060cdcc43cceb3670dad89d1a0cac9375c38c9c
profornnan <profornnan@naver.com>
2020-11-20 02:50:14 +0900

    chore: ESLint, Prettier, TypeScript 설정
    - .eslintrc.js 파일 추가
    - .prettierrc 파일 추가
    - tsconfig.json 파일 추가

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:@typescript-eslint/eslint-recommended', 'airbnb-base', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  ignorePatterns: ['dist/', 'node_modules/'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    '@typescript-eslint/no-explicit-any': 0,
+    'prefer-const': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0
+  }
+};
