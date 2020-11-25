e2cfc94bd74dcde011217833f1bd59cc519da1f4
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-25 11:16:27 +0900

    fix: index.d.ts의 인식이 되지 않는 현상 해결
    - index.ts가 index.d.ts와 겹치면서 인식이 되지 않는 현상 해결

--- client/public/imgs/index.ts
+++
@@ -1,4 +0,0 @@
-import * as Logo from './logo.png';
-import * as LogoText from './logo-text.png';
-
-export { Logo, LogoText };
--- client/src/components/atoms/ProfileImg/ProfileImg.tsx
+++ client/src/components/atoms/ProfileImg/ProfileImg.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Logo } from '@imgs';
+import Logo from '@imgs/logo.png';
 
 interface ProfileImgProps {
   size?: 'small' | 'medium' | 'large';
