d5006ce69dba44017e8ce0f13cf22cf9183ea5fc
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-25 10:49:27 +0900

    refactor: ProgressPlugin 제거 및 중괄호 제거
    - 개발 간 발생한 코딩 오류에 대한 수정

--- client/src/components/atoms/ProfileImg/ProfileImg.tsx
+++ client/src/components/atoms/ProfileImg/ProfileImg.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import Logo from '@imgs/logo.png';
+import { Logo } from '@imgs';
 
 interface ProfileImgProps {
   size?: 'small' | 'medium' | 'large';
   isHover?: boolean;
   src?: string;
+  isActive?: boolean;
 }
 
 const ProfileImgContainter = styled.div<ProfileImgProps>`
+  display: absolute;
   width: ${(props) => {
     if (props.size === 'large') return '2.2rem';
     if (props.size === 'medium') return '1.4rem';
     return '1.1rem';
-  }}}
+  }};
   height: ${(props) => {
     if (props.size === 'large') return '2.2rem';
     if (props.size === 'medium') return '1.4rem';
     return '1.1rem';
-  }}}
+  }};
   border-radius: ${(props) => {
     if (props.size === 'large') return '0.5rem';
     if (props.size === 'medium') return '0.3rem';
     return '0.2rem';
-  }}}
+  }};
   background-color: black;
 `;
 
