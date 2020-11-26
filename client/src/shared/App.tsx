f5cf7785b13b2f686234e266703c8f85d4ecdf5b
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-26 18:23:50 +0900

    feat: react-router 설정
    - 임시 페이지 생성
    - 헤더, 사이드바 컴포넌트 적용

--- client/src/shared/App.tsx
+++ client/src/shared/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Chatroom, Login } from '@pages/index';
+import { Header, Sidebar } from '@components/organisms';
+import { Channel, DM, Section } from '@components/molecules';
+
+const Flexbox = styled.img<any>`
+  display: flex;
+`;
 
 const App = () => {
-  return <div>Hello React!</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Sidebar>
+        <Section SectionName="Channels">
+          <Channel>5주-그룹-프로젝트-슬랙</Channel>
+          <Channel>boost-ajae</Channel>
+          <Channel>어몽어스</Channel>
+        </Section>
+        <Section SectionName="Direct Messages">
+          <DM>J003_강동훈</DM>
+          <DM>J030_김도호</DM>
+          <DM>J211_탁성건</DM>
+        </Section>
+      </Sidebar>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/" exact component={Chatroom} />
+        <Redirect path="*" to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
