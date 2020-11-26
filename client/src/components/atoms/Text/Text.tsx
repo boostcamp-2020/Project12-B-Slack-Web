b1604517c4fbbfe6f3e01024a2bfcd40c0a92593
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-26 15:51:19 +0900

    feat: Icon, Text 속성 추가
    - 여러 상황에 따른 Text와 Icon을 처리하기 위해 속성 추가

--- client/src/components/atoms/Text/Text.tsx
+++ client/src/components/atoms/Text/Text.tsx
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 interface TextProps {
   size?: 'small' | 'medium' | 'large';
   children: React.ReactChild;
-  isBold: boolean;
+  isBold?: boolean;
+  isSelect?: boolean;
+  isTitle?: boolean;
 }
 
 const StyledText = styled.p<any>`
-  color: rgb(198, 199, 200);
+  color: ${(props) => (props.isSelect || props.isTitle ? 'white' : 'rgb(198, 199, 200)')};
   font-size: ${(props) => {
     if (props.size === 'large') return '1.5rem';
     if (props.size === 'medium') return '1.3rem';
@@ -18,9 +20,9 @@ const StyledText = styled.p<any>`
   margin: 0;
 `;
 
-const Text: React.FC<TextProps> = ({ children, size = 'medium', isBold = false, ...props }) => {
+const Text: React.FC<TextProps> = ({ children, size = 'medium', isTitle = false, isBold = false, isSelect = false, ...props }) => {
   return (
-    <StyledText size={size} isBold={isBold} {...props}>
+    <StyledText size={size} isTitle={isTitle} isBold={isBold} isSelect={isSelect} {...props}>
       {children}
     </StyledText>
   );
