dfd37722a75c5b7afac550b7f7bb85651897ed16
Kang Dong Hoon <ypd01018@naver.com>
2020-11-26 14:38:48 +0900

    feat : OAuth API 추가 라우터 추가 패스포트 설정 추가 OAuth controller 추가 관련해서 model, userService 에 코드 추가

--- server/src/common/config/passport.ts
+++ server/src/common/config/passport.ts
@@ -4,12 +4,10 @@ const GitHubStrategy = require('passport-github').Strategy;
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
-    // console.log('serializeUser', user);
-    done(null, user.id);
+    done(null, user);
   });
 
   passport.deserializeUser((user, done) => {
-    // console.log('deserializeUser');
     done(null, user);
   });
 
@@ -20,11 +18,7 @@ module.exports = () => {
         clientSecret: 'efc948fa6c7f8f29a31309c774f21c2749d5ea85',
         callbackURL: 'http://127.0.0.1:3000/oauth/github/callback'
       },
-      (accessToken, refreshToken, profile, cb) => {
-        // console.log(accessToken, refreshToken, profile);
-        // User.findOrCreate({ githubId: profile.id }, function (err, user) {
-        //   return cb(err, user);
-        // });
+      async (accessToken, refreshToken, profile, cb) => {
         return cb(null, profile);
       }
     )
---
+++ server/src/controller/oauth-controller.ts
@@ -0,0 +1,37 @@
+import HttpStatusCode from '@constants/http-status-code';
+import User from '@model/user';
+import UserService from '@service/user-service';
+import { NextFunction, Request, Response } from 'express';
+
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+interface user {
+  username: string;
+}
+declare module 'express' {
+  interface Request {
+    user?: user;
+  }
+}
+const OauthController = {
+  async OauthCallback(req: Request, res: Response) {
+    try {
+      const userInfo = await UserService.getInstance().getUserById(String(req.user.username));
+    } catch {
+      const newUser = await UserService.getInstance().createUser(String(req.user.username));
+    }
+    const token = jwt.sign(
+      {
+        userId: req.user.username
+      },
+      'SECRET',
+      { expiresIn: 60 }
+    );
+    res.cookie('jwt', token);
+    res.send('login success');
+    // res.redirect('/');
+  }
+};
+
+export default OauthController;
