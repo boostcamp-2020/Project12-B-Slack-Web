35820ae51986015cb4cedbae7b5cb0aca37c19fa
profornnan <profornnan@naver.com>
2020-11-26 20:22:04 +0900

    feat: chatroom-repository에 findByTitle 추가 구현

--- server/src/repository/chatroom-repository.ts
+++ server/src/repository/chatroom-repository.ts
@@ -2,4 +2,8 @@ import { EntityRepository, Repository } from 'typeorm';
 import Chatroom from '@model/chatroom';
 
 @EntityRepository(Chatroom)
-export default class ChatroomRepository extends Repository<Chatroom> {}
+export default class ChatroomRepository extends Repository<Chatroom> {
+  findByTitle(title: string) {
+    return this.findOne({ title });
+  }
+}
