449ed1050603df48610f8ccd451bfe01c7ad612e
STak <59037261+profornnan@users.noreply.github.com>
2020-11-30 21:57:42 +0900

    feat: 채팅방 API 구현 (정보 조회, 정보 변경, 삭제 API) (#72)
    * feat: 채팅방 정보 조회 API 구현
    
    - 채팅방 정보를 조회하는 서비스, 컨트롤러 함수 작성
    - 채팅방 정보 조회 API 라우터 추가
    
    * feat: 채팅방 정보 변경 API 구현
    
    - 채팅방 정보를 변경하는 서비스, 컨트롤러 함수 작성
    - 채팅방 정보 변경 API 라우터 추가
    
    * feat: 채팅방 삭제 API 구현
    
    - 채팅방을 삭제하는 서비스, 컨트롤러 함수 작성
    - 채팅방 삭제 API 라우터 추가

--- server/src/router/chatroom-router.ts
+++ server/src/router/chatroom-router.ts
@@ -4,5 +4,8 @@ import ChatroomController from '@controller/chatroom-controller';
 const router = express.Router();
 
 router.post('/', ChatroomController.createChatroom);
+router.get('/:chatroomId', ChatroomController.getChatroomInfo);
+router.patch('/:chatroomId', ChatroomController.updateChatroom);
+router.delete('/:chatroomId', ChatroomController.deleteChatroom);
 
 export default router;
