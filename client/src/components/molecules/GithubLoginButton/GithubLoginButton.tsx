007f56ebea2fdbe9d68d4421a29c09e4a2041df1
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-30 13:10:12 +0900

    feat: GithubLoginButton component 구현

---
+++ client/src/components/molecules/GithubLoginButton/GithubLoginButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Icon, Text } from '@components/atoms';
+import GithubLoginImg from '@imgs/github-login.png';
+import styled from 'styled-components';
+
+interface GithubLoginButtonProps {
+  size?: 'small' | 'large';
+}
+
+const TextWrap = styled.div`
+  margin-left: 1rem;
+`;
+
+const GithubLoginButton: React.FC<GithubLoginButtonProps> = ({ ...props }) => {
+  return (
+    <Button backgroundColor="black" borderColor="black" fontColor="black" {...props}>
+      <Icon src="https://user-images.githubusercontent.com/32856129/98276858-ef6a0880-1fd9-11eb-9473-c8844c066cce.png" isHover={false} />
+      <TextWrap>
+        <Text color="white"> Github Login </Text>
+      </TextWrap>
+    </Button>
+  );
+};
+
+export { GithubLoginButton, GithubLoginButtonProps };
