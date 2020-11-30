9e74ac180bec4e0b74bc64e54ea7685b5c36bc19
Kang Dong Hoon <ypd01018@naver.com>
2020-11-30 19:02:33 +0900

    feat : 채팅방 모든 메시지 조회 api 구현 단일 메시지 조회 라우터 삭제

--- server/src/router/message-router.ts
+++ server/src/router/message-router.ts
@@ -3,7 +3,8 @@ import messageController from '@controller/message-controller';
 
 const router = express.Router();
 
-router.get('/:messageId', messageController.getMessage);
+// router.get('/:messageId', messageController.getMessage);
+router.get('/:chatRoomId', messageController.getMessages);
 router.post('/', messageController.createMessage);
 router.patch('/:messageId', messageController.updateMessage);
 router.delete('/:messageId', messageController.deleteMessage);
