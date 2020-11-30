a15f9ec1bb5d2578f84eb08366058f7262c64369
Do-ho <hong7511059@koreatech.ac.kr>
2020-11-30 13:09:53 +0900

    style: Button style 개선

--- client/src/components/atoms/Button/Button.tsx
+++ client/src/components/atoms/Button/Button.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ButtonProps {
-  children: React.ReactChild;
-  width: string;
-  height: string;
+  children: React.ReactNode;
   backgroundColor: string;
   borderColor: string;
   fontColor: string;
 }
 
 const StyledButton = styled.button<any>`
+  display: flex;
+  align-items: center;
   background-color: ${(props) => props.backgroundColor};
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
   border: 2px solid ${(props) => props.borderColor};
   color: ${(props) => props.fontColor};
   padding: 0.3rem 1rem;
+  border-radius: 0.4rem;
+  outline: none;
+  cursor: pointer;
 `;
 
-const Button: React.FC<ButtonProps> = ({ children, width, height, backgroundColor, borderColor, fontColor, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ children, backgroundColor, borderColor, fontColor, ...props }) => {
   return (
-    <StyledButton width={width} height={height} backgroundColor={backgroundColor} borderColor={borderColor} fontColor={fontColor} {...props}>
+    <StyledButton backgroundColor={backgroundColor} borderColor={borderColor} fontColor={fontColor} {...props}>
       {children}
     </StyledButton>
   );
