4833d4d1f7b807c5d6cb1f33c186bdbb057e4215
Kang Dong Hoon <ypd01018@naver.com>
2020-12-02 19:11:35 +0900

    feat: 채팅방 메시지 조회 api 구현

--- server/src/service/message-service.ts
+++ server/src/service/message-service.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import MessageRepository from '@repository/message-repository';
 import UserRepository from '@repository/user-repository';
 import ChatroomRepository from '@repository/chatroom-repository';
+
 import validator from '../common/utils/validator';
 
 class MessageService {
@@ -43,12 +44,29 @@ class MessageService {
     return message;
   }
 
-  async getMessages(chatroomId: number) {
+  async getMessages(chatroomId: number, offsetId: number) {
     const messages = await this.MessageRepository.createQueryBuilder('message')
       .leftJoin('message.user', 'user')
-      .select(['message', 'user.userId', 'user.profileUri', 'user.displayName'])
-      .where('message.chatroomId = :chatroomId', { chatroomId })
+      .leftJoin('message.replies', 'replies')
+      .leftJoin('replies.user', 'replyUser')
+      .leftJoin('message.messageReactions', 'messageReactions')
+      .leftJoin('messageReactions.reaction', 'reaction')
+      .leftJoin('messageReactions.user', 'reactionUser')
+      .select(['message.messageId', 'message.createdAt', 'message.updatedAt'])
+      .addSelect(['user.userId', 'user.profileUri', 'user.displayName'])
+      .addSelect(['messageReactions.messageReactionId'])
+      .addSelect(['reaction.reactionId', 'reaction.title', 'reaction.imageUri'])
+      .addSelect(['reactionUser.displayName'])
+      .addSelect(['replies.createdAt'])
+      .addSelect(['replyUser.profileUri'])
+      .orderBy('message.messageId', 'DESC')
+      .limit(10)
+      .where('message.chatroomId = :chatroomId AND message.messageId < :offsetId', { chatroomId, offsetId })
       .getMany();
+
+    this.customMessagesReaction(messages);
+    this.customMessagesReplies(messages);
+
     return messages;
   }
 
@@ -61,6 +79,47 @@ class MessageService {
 
   async deleteMessage(messageId: number) {
     await this.MessageRepository.softDelete(messageId);
+  }
+
+  private async customMessagesReaction(messages: any) {
+    messages.forEach((message: any) => {
+      let messageReactions: any = {};
+      message.messageReactions.forEach((messageReaction) => {
+        if (messageReactions[messageReaction.reaction.reactionId])
+          messageReactions[messageReaction.reaction.reactionId].replyDisplayNames.push(messageReaction.user.displayName);
+        else
+          messageReactions[messageReaction.reaction.reactionId] = {
+            reactionId: messageReaction.reaction.reactionId,
+            title: messageReaction.reaction.title,
+            imageUri: messageReaction.reaction.imageUri,
+            replyDisplayNames: [messageReaction.user.displayName]
+          };
+      });
+      message.messageReactions = messageReactions;
+    });
+    return messages;
+  }
+
+  private async customMessagesReplies(messages: any) {
+    messages.forEach((message: any) => {
+      let lastReplyAtNumber = 0;
+      const replyCount = message.replies.length;
+      const profileUris = [];
+      message.replies.forEach((reply: any) => {
+        lastReplyAtNumber = Math.max(reply.createdAt);
+        if (profileUris.length < 5) profileUris.push(reply.user.profileUri);
+      });
+      console.log(lastReplyAtNumber);
+      const lastReplyAt = lastReplyAtNumber === 0 ? undefined : new Date(lastReplyAtNumber);
+      delete message.replies;
+      message.thread = {
+        lastReplyAt,
+        replyCount,
+        profileUris
+      };
+    });
+
+    return messages;
   }
 }
 
