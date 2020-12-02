b3eba790cf3b75053c9610a05b3732ae92c173d3
STak <59037261+profornnan@users.noreply.github.com>
2020-12-02 13:14:04 +0900

    feat: Reply API 구현 (댓글 생성, 단일 조회, 부분 조회 API) (#79)
    * feat: 댓글 생성 서비스 함수 작성
    
    * feat: 댓글 생성 컨트롤러 함수 작성
    
    * feat: reply model에 validation annotation 추가
    
    - content에 @IsString() 추가
    
    * feat: 댓글 생성 API 라우터 추가
    
    * feat: 댓글 단일 조회 API 구현
    
    - 단일 댓글을 조회하는 서비스, 컨트롤러 함수 작성
    - 댓글 단일 조회 API 라우터 추가
    
    * feat: 댓글 전체 조회 API 구현 (offsetId 이용)
    
    - offsetId를 이용해 댓글들을 조회하는 서비스, 컨트롤러 함수 작성
    - offsetId를 이용한 댓글 조회 API 라우터 추가
    - offsetId가 없을 경우 최신 댓글 정보를 조회하도록 구현
    - 단일 댓글 조회 API 라우터 제거
    
    * feat: 댓글 생성 시 replyId 정보 반환
    
    - 댓글 생성 시 생성된 댓글의 replyId를 반환하도록 수정

---
+++ server/src/controller/reply-controller.ts
@@ -0,0 +1,40 @@
+import HttpStatusCode from '@constants/http-status-code';
+import { NextFunction, Request, Response } from 'express';
+import ReplyService from '@service/reply-service';
+
+const ReplyController = {
+  async createReply(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.user;
+    const { messageId, content } = req.body;
+    try {
+      const replyId = await ReplyService.getInstance().createReply(Number(userId), Number(messageId), content);
+      res.status(HttpStatusCode.CREATED).json({ replyId });
+    } catch (err) {
+      next(err);
+    }
+  },
+  async getReply(req: Request, res: Response, next: NextFunction) {
+    const { replyId } = req.params;
+    try {
+      const reply = await ReplyService.getInstance().getReply(Number(replyId));
+      res.status(HttpStatusCode.OK).json(reply);
+    } catch (err) {
+      next(err);
+    }
+  },
+  async getReplies(req: Request, res: Response, next: NextFunction) {
+    const { offsetId } = req.params;
+    try {
+      if (!offsetId) {
+        const recentReplies = await ReplyService.getInstance().getRecentReplies();
+        res.status(HttpStatusCode.OK).json(recentReplies);
+      }
+      const replies = await ReplyService.getInstance().getRepliesByOffsetId(Number(offsetId));
+      res.status(HttpStatusCode.OK).json(replies);
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+
+export default ReplyController;
