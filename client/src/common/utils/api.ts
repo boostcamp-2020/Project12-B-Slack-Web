1a2a757cc09c1b2a9cd7d90e03b86094a7cc43b7
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-02 20:59:38 +0900

    feat: getToken API 로직 수정

--- client/src/common/utils/api.ts
+++ client/src/common/utils/api.ts
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://127.0.0.1:3000';
 
-const getToken = async (code: string) => {
+const getToken = async (code: string | null) => {
   const response = await axios.get(`/oauth/github/${code}`);
   if (response.headers.authorization) {
     axios.defaults.headers.common.Authorization = response.headers.authorization;
+    return response.headers.authorization;
   }
+  return null;
 };
 
 const oauthLogin = async () => {
