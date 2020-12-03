7097bd27609668c4b03d35ffead69f49b2363e88
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-03 11:24:04 +0900

    refactor: store 기본 값 설정 및 switch문으로 교체

--- client/src/common/store/user-store.ts
+++ client/src/common/store/user-store.ts
@@ -1,14 +1,20 @@
 import { createStore } from 'redux';
 
 export default createStore((state: any, action: any) => {
-  if (state === undefined) {
-    return { isLogin: false };
+  const initialInfo = {
+    userId: 0,
+    profileUri: '',
+    displayName: ''
+  };
+
+  if (state === undefined) return initialInfo;
+
+  switch (action.type) {
+    case 'LOGIN':
+      return { ...state, userId: action.userId, profileUri: action.profileUri, displayName: action.displayName };
+    case 'LOGOUT':
+      return { ...state, ...initialInfo };
+    default:
+      return state;
   }
-  if (action.type === 'LOGIN') {
-    return { ...state, isLogin: true };
-  }
-  if (action.type === 'LOGOUT') {
-    return { ...state, isLogin: false };
-  }
-  return state;
 });
