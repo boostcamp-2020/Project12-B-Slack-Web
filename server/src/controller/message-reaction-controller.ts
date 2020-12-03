5ad5e08fb4002554b5e88d056d1b0c969707db35
STak <59037261+profornnan@users.noreply.github.com>
2020-12-03 16:39:41 +0900

    feat: 메시지 리액션 추가 API, 메시지 리액션 제거 API 구현 (#90)
    * feat: 메시지 리액션 추가 API 구현
    
    - 메시지에 리액션을 추가하는 서비스, 컨트롤러 함수 작성
    - 메시지 리액션 추가 API 라우터 추가
    
    * feat: 메시지 리액션 제거 API 구현
    
    - 메시지 리액션을 제거하는 서비스, 컨트롤러 함수 작성
    - 메시지 리액션 제거 API 라우터 추가

---
+++ server/src/controller/message-reaction-controller.ts
@@ -0,0 +1,32 @@
+import HttpStatusCode from '@constants/http-status-code';
+import { NextFunction, Request, Response } from 'express';
+import MessageReactionService from '@service/message-reaction-service';
+
+const MessageReactionController = {
+  async createMessageReaction(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.user;
+    const { messageId, reactionId } = req.body;
+    try {
+      const messageReactionId = await MessageReactionService.getInstance().createMessageReaction(
+        Number(userId),
+        Number(messageId),
+        Number(reactionId)
+      );
+      res.status(HttpStatusCode.CREATED).json({ messageReactionId });
+    } catch (err) {
+      next(err);
+    }
+  },
+  async deleteMessageReaction(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.user;
+    const { messageId, reactionId } = req.params;
+    try {
+      await MessageReactionService.getInstance().deleteMessageReaction(Number(userId), Number(messageId), Number(reactionId));
+      res.status(HttpStatusCode.NO_CONTENT).send();
+    } catch (err) {
+      next(err);
+    }
+  }
+};
+
+export default MessageReactionController;
---
+++ server/src/service/message-reaction-service.ts
@@ -0,0 +1,71 @@
+import { getCustomRepository } from 'typeorm';
+import MessageReactionRepository from '@repository/message-reaction-repository';
+import UserRepository from '@repository/user-repository';
+import MessageRepository from '@repository/message-repository';
+import ReactionRepository from '@repository/reacion-repository';
+import BadRequestError from '@error/bad-request-error';
+import ConflictError from '@error/conflict-error';
+import NotFoundError from '@error/not-found-error';
+
+class MessageReactionService {
+  static instance: MessageReactionService;
+
+  messageReactionRepository: MessageReactionRepository;
+
+  userRepository: UserRepository;
+
+  messageRepository: MessageRepository;
+
+  reactionRepository: ReactionRepository;
+
+  constructor() {
+    this.messageReactionRepository = getCustomRepository(MessageReactionRepository);
+    this.userRepository = getCustomRepository(UserRepository);
+    this.messageRepository = getCustomRepository(MessageRepository);
+    this.reactionRepository = getCustomRepository(ReactionRepository);
+  }
+
+  static getInstance(): MessageReactionService {
+    if (!MessageReactionService.instance) {
+      MessageReactionService.instance = new MessageReactionService();
+    }
+    return MessageReactionService.instance;
+  }
+
+  async createMessageReaction(userId: number, messageId: number, reactionId: number) {
+    const user = await this.userRepository.findOne({ userId });
+    const message = await this.messageRepository.findOne({ messageId });
+    const reaction = await this.reactionRepository.findOne({ reactionId });
+
+    if (!user || !message || !reaction) {
+      throw new BadRequestError();
+    }
+
+    const newMessageReaction = this.messageReactionRepository.create({ user, message, reaction });
+    const messageReaction = await this.messageReactionRepository.findOne(newMessageReaction);
+
+    if (messageReaction) {
+      throw new ConflictError();
+    }
+
+    const createdMessageReaction = await this.messageReactionRepository.save(newMessageReaction);
+    return createdMessageReaction.messageReactionId;
+  }
+
+  async deleteMessageReaction(userId: number, messageId: number, reactionId: number) {
+    const messageReaction = await this.messageReactionRepository
+      .createQueryBuilder('messageReaction')
+      .where('messageReaction.userId = :userId', { userId })
+      .andWhere('messageReaction.messageId = :messageId', { messageId })
+      .andWhere('messageReaction.reactionId = :reactionId', { reactionId })
+      .getOne();
+
+    if (!messageReaction) {
+      throw new NotFoundError();
+    }
+
+    await this.messageReactionRepository.softDelete(messageReaction.messageReactionId);
+  }
+}
+
+export default MessageReactionService;
