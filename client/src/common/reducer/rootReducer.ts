241c84c57d0a5559a64e3f4e58e5c6e9c6285d1d
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-04 00:03:17 +0900

    feat: chatroom reducer 정의

--- client/src/common/reducer/rootReducer.ts
+++ client/src/common/reducer/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './user-reducer';
 import sidebarReducer from './sidebar-reducer';
+import chatroomReducer from './chatroom-reducer';
 
 const rootReducer = combineReducers({
   userData: userReducer,
   sidebarData: sidebarReducer,
+  chatroomData: chatroomReducer
 });
 
 export default rootReducer;
