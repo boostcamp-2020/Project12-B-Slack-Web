f6477d4c534e3a861b7e17f463fd108e7b03ac5c
Kang Dong Hoon <ypd01018@naver.com>
2020-12-03 16:50:43 +0900

    feat : user 가입시 default channel, dm 들어가도록 설정

--- server/src/service/user-service.ts
+++ server/src/service/user-service.ts
@@ -1,14 +1,24 @@
 import { getCustomRepository } from 'typeorm';
 import UserRepository from '@repository/user-repository';
+import ChatroomRepository from '@repository/chatroom-repository';
+import UserChatroomRepository from '@repository/user-chatroom-repository';
 import NotFoundError from '@error/not-found-error';
+import Chatroom from '@model/chatroom';
+import User from '@model/user';
 
 class UserService {
   static instance: UserService;
 
   userRepository: UserRepository;
 
+  chatroomRepository: ChatroomRepository;
+
+  userChatroomRepository: UserChatroomRepository;
+
   constructor() {
     this.userRepository = getCustomRepository(UserRepository);
+    this.chatroomRepository = getCustomRepository(ChatroomRepository);
+    this.userChatroomRepository = getCustomRepository(UserChatroomRepository);
   }
 
   static getInstance(): UserService {
@@ -59,6 +69,24 @@ class UserService {
 
   async createUser(id: string) {
     const newUser = await this.userRepository.save({ id, fullName: id, displayName: id, isSocial: true });
+    const newDm = await this.createDm();
+    await this.joinDm(newUser, newDm);
+    await this.joinDefaultChannel(newUser);
+  }
+
+  private async createDm() {
+    const newDm = await this.chatroomRepository.save({ isPrivate: true, chatType: 'DM' });
+    return newDm;
+  }
+
+  private async joinDm(user: User, chatroom: Chatroom) {
+    await this.userChatroomRepository.save({ user, chatroom, sectionName: 'DM' });
+  }
+
+  private async joinDefaultChannel(user: User) {
+    const title = 'random';
+    const chatroom = await this.chatroomRepository.findOne({ title });
+    await this.userChatroomRepository.save({ user, chatroom, sectionName: 'Channel' });
   }
 }
 
