91af5729f478cfface35dbe8397fc85c650506d6
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-03 21:23:47 +0900

    refactor: Sidebar 채팅방 생성 함수 리팩토링

--- client/src/components/organisms/Sidebar/Sidebar.tsx
+++ client/src/components/organisms/Sidebar/Sidebar.tsx
@@ -32,45 +32,30 @@ const ChildrenWrap = styled.div<any>`
 `;
 
 const Sidebar: React.FC<SidebarProps> = ({ children, starred, otherSections, channels, directMessages, selectedChatroomId, ...props }) => {
-  const makeStarredSection = () => {
-    if (starred.length === 0) return null;
-    const chatrooms = starred.map((chatroom) => (
-      <Channel
-        chatroomId={chatroom.chatroomId}
-        key={chatroom.chatroomId}
-        isPrivate={chatroom.isPrivate}
-        isSelect={selectedChatroomId === chatroom.chatroomId}>
-        {chatroom.title}
-      </Channel>
-    ));
-    return <Section SectionName="starred">{chatrooms}</Section>;
+  const createSection = (sectionItems: Array<any>, sectionName: string) => {
+    if (sectionItems.length === 0) return null;
+    const chatrooms = sectionItems.map((chatroom) =>
+      chatroom.chatType === 'Channel' ? (
+        <Channel
+          chatroomId={chatroom.chatroomId}
+          key={chatroom.chatroomId}
+          isPrivate={chatroom.isPrivate}
+          isSelect={selectedChatroomId === chatroom.chatroomId}>
+          {chatroom.title}
+        </Channel>
+      ) : (
+        <DM
+          src={chatroom.chatProfileImg}
+          chatroomId={chatroom.chatroomId}
+          key={chatroom.chatroomId}
+          isSelect={selectedChatroomId === chatroom.chatroomId}>
+          {chatroom.title}
+        </DM>
+      )
+    );
+    return <Section SectionName={sectionName}>{chatrooms}</Section>;
   };
-  const makeChannelSection = () => {
-    if (channels.length === 0) return null;
-    const chatrooms = channels.map((chatroom) => (
-      <Channel
-        chatroomId={chatroom.chatroomId}
-        key={chatroom.chatroomId}
-        isPrivate={chatroom.isPrivate}
-        isSelect={selectedChatroomId === chatroom.chatroomId}>
-        {chatroom.title}
-      </Channel>
-    ));
-    return <Section SectionName="Channels">{chatrooms}</Section>;
-  };
-  const makeDMSection = () => {
-    if (directMessages.length === 0) return null;
-    const chatrooms = directMessages.map((chatroom) => (
-      <DM
-        src={chatroom.chatProfileImg}
-        chatroomId={chatroom.chatroomId}
-        key={chatroom.chatroomId}
-        isSelect={selectedChatroomId === chatroom.chatroomId}>
-        {chatroom.title}
-      </DM>
-    ));
-    return <Section SectionName="Direct Messages">{chatrooms}</Section>;
-  };
+
   return (
     <StyledSidebar {...props}>
       <Workspace>
@@ -79,9 +64,9 @@ const Sidebar: React.FC<SidebarProps> = ({ children, starred, otherSections, cha
         </Text>
       </Workspace>
       <ChildrenWrap>
-        {makeStarredSection()}
-        {makeChannelSection()}
-        {makeDMSection()}
+        {createSection(starred, 'Starred')}
+        {createSection(channels, 'Channels')}
+        {createSection(directMessages, 'Direct Messages')}
       </ChildrenWrap>
     </StyledSidebar>
   );
