c595876b318f669c72796740b3a787efb32f3163
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-03 20:24:16 +0900

    refactor: LoginForm Organism으로 분리

---
+++ client/src/components/organisms/LoginForm/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, LogoImg } from '@components/atoms';
+import { GithubLoginButton } from '@components/molecules';
+import styled from 'styled-components';
+
+interface LoginFormProps {}
+
+const StyledLoginForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 17rem;
+`;
+
+const LoginForm: React.FC<LoginFormProps> = () => {
+  return (
+    <StyledLoginForm>
+      <LogoImg size="large" />
+      <Text size="big" color="black" isBold={true}>
+        Black에 로그인
+      </Text>
+      <Text color="rgb(160, 158, 169)" isBold={true}>
+        로그인하려면 사용하는 Github 계정으로 계속해 주세요.
+      </Text>
+      <GithubLoginButton />
+    </StyledLoginForm>
+  );
+};
+
+export { LoginForm, LoginFormProps };
