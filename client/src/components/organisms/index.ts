8d4bab4c2c5389b87bd7ee51eeb4b57df88226c7
profornnan <profornnan@naver.com>
2020-12-07 18:35:44 +0900

    feat: organisms/index.ts에 컴포넌트 추가
    - BrowsePageChannel

--- client/src/components/organisms/index.ts
+++ client/src/components/organisms/index.ts
@@ -3,5 +3,6 @@ import Sidebar from './Sidebar/SidebarContainer';
 import { ChatroomHeader } from './ChatroomHeader/ChatroomHeader';
 import { ChatroomBody } from './ChatroomBody/ChatroomBody';
 import { LoginForm } from './LoginForm/LoginForm';
+import { BrowsePageChannel } from './BrowsePageChannel/BrowsePageChannel';
 
-export { Header, Sidebar, ChatroomHeader, ChatroomBody, LoginForm };
+export { Header, Sidebar, ChatroomHeader, ChatroomBody, LoginForm, BrowsePageChannel };
