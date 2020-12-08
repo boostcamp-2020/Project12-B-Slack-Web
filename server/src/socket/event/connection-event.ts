cc3236bf124ba82161f95493c8e5750cd6713d9d
Kang Dong Hoon <ypd01018@naver.com>
2020-12-08 14:02:39 +0900

    refactor: 불필요한 출력 삭제

--- server/src/socket/event/connection-event.ts
+++ server/src/socket/event/connection-event.ts
@@ -1,8 +1,6 @@
 import chatroomHandler from '@socket/handler/chatroom-handler';
 
 const connectionEvent = (io, socket) => {
-  const req = socket.request;
-  console.log('새로운 클라이언트', req.user);
   chatroomHandler.joinChatroom(io, socket);
 };
 
