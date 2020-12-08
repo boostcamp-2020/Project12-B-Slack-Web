49edff6b7c0e47aded785022a97b4525429dd367
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-08 23:22:11 +0900

    refactor: 연동과정 server api 변경

--- server/src/router/message-router.ts
+++ server/src/router/message-router.ts
@@ -3,8 +3,9 @@ import messageController from '@controller/message-controller';
 
 const router = express.Router();
 
-router.get('/:chatRoomId/:offsetId', messageController.getMessages);
+// router.get('/:chatRoomId/:offsetId', messageController.getMessages);
 router.get('/:chatRoomId', messageController.getMessages);
+router.get('/:chatRoomId/:messageId', messageController.getMessage);
 router.post('/', messageController.createMessage);
 router.patch('/:messageId', messageController.updateMessage);
 router.delete('/:messageId', messageController.deleteMessage);
