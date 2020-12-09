6be0f8926eab3607cb502c4d7e8ecb1f7bc2320e
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-09 16:21:52 +0900

    feat: Enter 누를 시 Message 추가
    - keypress이벤트를 이용해 구현

--- client/src/components/atoms/Input/Input.tsx
+++ client/src/components/atoms/Input/Input.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const ENTER_KEYCODE = 13;
+
 interface InputProps {
   title?: string;
   isThread?: boolean;
   content: string;
   setContent: any;
+  keyPressEnter: any;
 }
 
 const StyledInput = styled.input<any>`
@@ -15,11 +18,22 @@ const StyledInput = styled.input<any>`
   outline: none;
 `;
 
-const Input: React.FC<InputProps> = ({ title, isThread = false, content, setContent, ...props }) => {
+const Input: React.FC<InputProps> = ({ title, isThread = false, content, setContent, keyPressEnter, ...props }) => {
+  const handlingKeyPressEnter = (e: any) => {
+    if (e.charCode === ENTER_KEYCODE) keyPressEnter(e.target.value);
+  };
   const handlingChange = (e: any) => {
     setContent(e.target.value);
   };
-  return <StyledInput placeholder={isThread ? 'Reply...' : `Send a message to #${title}`} value={content} onChange={handlingChange} {...props} />;
+  return (
+    <StyledInput
+      placeholder={isThread ? 'Reply...' : `Send a message to #${title}`}
+      value={content}
+      onKeyPress={handlingKeyPressEnter}
+      onChange={handlingChange}
+      {...props}
+    />
+  );
 };
 
 export { Input, InputProps };
