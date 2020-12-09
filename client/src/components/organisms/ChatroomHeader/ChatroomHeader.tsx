b0174bd24f4c4dc3751ad60dc77710d070f7b207
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-09 15:48:21 +0900

    refactor: color 속성 theme 적용

--- client/src/components/organisms/ChatroomHeader/ChatroomHeader.tsx
+++ client/src/components/organisms/ChatroomHeader/ChatroomHeader.tsx
@@ -18,7 +18,7 @@ const ChatroomHeaderContainter = styled.div<any>`
   align-items: center;
   height: 10%;
   width: 100%;
-  box-shadow: 0 2px 2px -2px #e2e2e2;
+  box-shadow: 0 2px 2px -2px ${color.border_primary};
   background-color: ${color.tertiary};
   z-index: 2;
 `;
