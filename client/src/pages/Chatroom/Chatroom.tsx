79190e2a000d28c5641d41085936c28dffaf94df
profornnan <profornnan@naver.com>
2020-12-10 22:51:38 +0900

    feat: socket event listener가 여러번 등록되는 오류 해결
    - 페이지 이동 시 socket event listener가 여러번 등록되는 문제 발생
    - socket event listener 등록을 Body 컴포넌트에서 하도록 변경

--- client/src/pages/Chatroom/Chatroom.tsx
+++ client/src/pages/Chatroom/Chatroom.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 import { ChatroomHeader, ChatroomBody } from '@components/organisms';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@store/reducers/index';
-import { insertMessage, loadAsync } from '@store/actions/chatroom-action';
-import socket from '@socket/socketIO';
+import { loadAsync } from '@store/actions/chatroom-action';
 
 interface ChatroomProps {
   children: React.ReactNode;
@@ -18,12 +17,8 @@ const Chatroom: React.FC<ChatroomProps> = ({ children, ...props }) => {
   const dispatch = useDispatch();
   const { selectedChatroomId, selectedChatroom, messages } = useSelector((store: RootState) => store.chatroom);
   const { title, users } = selectedChatroom;
-
   useEffect(() => {
     dispatch(loadAsync({ selectedChatroomId }));
-    socket.on('create message', (message: any) => {
-      dispatch(insertMessage(message));
-    });
   }, []);
 
   return (
