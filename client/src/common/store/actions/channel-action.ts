96265c9d02a8042b5fa03eddf5674e0770263b2c
STak <59037261+profornnan@users.noreply.github.com>
2020-12-11 02:50:39 +0900

    feat: Channel Store 구현 (#176)
    * feat: Channel Store Type 정의
    
    * feat: Channel Store Action 정의
    
    * feat: getChannels API 구현
    
    - 초기 채널 정보를 받아오는 getChannels API 구현
    
    * feat: Channel Store Reducer, Saga 정의

---
+++ client/src/common/store/actions/channel-action.ts
@@ -0,0 +1,3 @@
+import { INIT_CHANNELS_ASYNC } from '../types/channel-types';
+
+export const initChannels = () => ({ type: INIT_CHANNELS_ASYNC });
--- client/src/common/store/reducers/index.ts
+++ client/src/common/store/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './user-reducer';
 import chatroomReducer from './chatroom-reducer';
 import modalReducer from './modal-reducer';
+import channelReducer from './channel-reducer';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   chatroom: chatroomReducer,
-  modal: modalReducer
+  modal: modalReducer,
+  channel: channelReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
