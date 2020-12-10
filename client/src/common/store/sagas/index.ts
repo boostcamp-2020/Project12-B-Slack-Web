1d6690b8d52970e033937f5f55d9a2091d7a11fb
profornnan <profornnan@naver.com>
2020-12-11 02:16:12 +0900

    feat: Channel Store Reducer, Saga 정의

--- client/src/common/store/sagas/index.ts
+++ client/src/common/store/sagas/index.ts
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import { chatroomSaga } from './chatroom-saga';
 import { userSaga } from './user-saga';
+import { channelSaga } from './channel-saga';
 
 export function* rootSaga() {
-  yield all([chatroomSaga(), userSaga()]);
+  yield all([chatroomSaga(), userSaga(), channelSaga()]);
 }
