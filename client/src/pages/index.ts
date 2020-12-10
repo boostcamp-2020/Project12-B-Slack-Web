f4512b5415ed52b824d292e2118b20f50c6ab930
Do Ho Kim <33643752+Do-ho@users.noreply.github.com>
2020-12-11 02:58:53 +0900

    fix: page index 누락 수정 (#178)
    - merge 과정 중 page index 누락된 부분 수정

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -5,4 +5,4 @@ import { Thread } from './Chatroom/Thread';
 import { LoginLoading } from './LoginLoading';
 import { ChannelBrowser } from './ChannelBrowser/ChannelBrowser';
 
-export { Chatroom, Login, LoginLoading, ChannelBrowser, Thread };
+export { Chatroom, Login, LoginLoading, ChannelBrowser, Thread, ChatroomThread };
