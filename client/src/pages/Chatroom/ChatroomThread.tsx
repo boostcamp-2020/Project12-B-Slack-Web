0d046479e2c249df9d64db738c44014755d7feec
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-11 02:39:27 +0900

    feat: Thread page 적용

--- client/src/pages/Chatroom/ChatroomThread.tsx
+++ client/src/pages/Chatroom/ChatroomThread.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Chatroom } from '@pages/index';
+import { Chatroom, Thread } from '@pages/index';
 
 interface ChatroomProps {
   children: React.ReactNode;
 }
 
 const ChatroomThreadContainer = styled.div<any>`
+  display: flex;
   height: 100%;
 `;
 
-const ChatroomThread: React.FC<ChatroomProps> = ({ children, ...props }) => {
+const ChatroomThread: React.FC<ChatroomProps> = () => {
   return (
     <ChatroomThreadContainer>
       <Chatroom />
+      <Thread />
     </ChatroomThreadContainer>
   );
 };
