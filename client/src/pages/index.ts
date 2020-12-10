51c9a1f1c0abbbe1d4e11205054261accc3065d4
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-11 02:39:01 +0900

    feat: Thread page 생성

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 import { Chatroom } from './Chatroom/Chatroom';
 import { Login } from './Login';
 import { ChatroomThread } from './Chatroom/ChatroomThread';
+import { Thread } from './Chatroom/Thread';
 
-export { Chatroom, Login, ChatroomThread };
+export { Chatroom, Login, ChatroomThread, Thread };
