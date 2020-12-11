cea8486dbbcf2f965d9ef5b0e429f26eb8faca3a
ypd01018 <37091190+ypd01018@users.noreply.github.com>
2020-12-11 13:17:03 +0900

    feat: 전체 chatroom 조회에 isJoined 프로퍼티, 헤더에 채널 전체 갯수 추가 구현 (#182)
    * feat: 전체 채널 조회 api에 isjoined 속성 추가
    
    * feat: getchatroom 헤더에 chatroom 갯수 추가
    
    * refactor: 코드 리뷰 반영

--- server/src/controller/chatroom-controller.ts
+++ server/src/controller/chatroom-controller.ts
@@ -27,7 +27,9 @@ const ChatroomController = {
     try {
       const { userId } = req.user;
       const { offsetTitle } = req.query;
-      const chatrooms = await ChatroomService.getInstance().getChatrooms(Number(userId), Number(offsetTitle));
+      const chatrooms = await ChatroomService.getInstance().getChatrooms(Number(userId), String(offsetTitle));
+      const chatroomCount = await ChatroomService.getInstance().getChatroomCount(userId);
+      res.setHeader('X-total-count', chatroomCount);
       res.status(HttpStatusCode.OK).json(chatrooms);
     } catch (err) {
       next(err);
