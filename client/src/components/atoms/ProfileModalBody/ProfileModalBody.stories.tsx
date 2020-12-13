87107578f2dba1c9ca733d70b777464bafb4ef99
profornnan <profornnan@naver.com>
2020-12-13 23:47:55 +0900

    feat: Profile Modal Body 컴포넌트 구현
    - Local time이 현재 시간에 맞춰 실시간으로 변하도록 구현

---
+++ client/src/components/atoms/ProfileModalBody/ProfileModalBody.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { ProfileModalBody, ProfileModalBodyProps } from './ProfileModalBody';
+
+export default {
+  title: 'atom/ProfileModalBody',
+  component: ProfileModalBody
+} as Meta;
+
+const Template: Story<ProfileModalBodyProps> = (args) => <ProfileModalBody {...args} />;
+
+export const BlackProfileModalBody = Template.bind({});
+BlackProfileModalBody.args = {
+  displayName: '탁성건'
+};
---
+++ client/src/components/atoms/ProfileModalBody/ProfileModalBody.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { color } from '@theme/index';
+import { ActiveLight, Text } from '@components/atoms';
+import { getTimeConversionValue } from '@utils/time';
+
+interface ProfileModalBodyProps {
+  displayName: string;
+}
+
+const ProfileModalBodyContainter = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
+
+const DisplayNameWrap = styled.div`
+  display: flex;
+  align-items: flex-end;
+  p {
+    margin-right: 0.5rem;
+  }
+`;
+
+const LocalTimeWrap = styled.div`
+  margin-top: 1rem;
+`;
+
+const ProfileModalBody: React.FC<ProfileModalBodyProps> = ({ displayName, ...props }) => {
+  const [localTime, setLocalTime] = useState(getTimeConversionValue(new Date()));
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setLocalTime(getTimeConversionValue(new Date()));
+    }, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [localTime]);
+
+  return (
+    <ProfileModalBodyContainter {...props}>
+      <DisplayNameWrap>
+        <Text fontColor={color.primary} size="medium" isBold={true}>
+          {displayName}
+        </Text>
+        <ActiveLight size="medium" isActive={true} />
+      </DisplayNameWrap>
+      <LocalTimeWrap>
+        <Text fontColor={color.text_tertiary} size="superSmall">
+          Local time
+        </Text>
+        <Text fontColor={color.primary} size="small">
+          {localTime}
+        </Text>
+      </LocalTimeWrap>
+    </ProfileModalBodyContainter>
+  );
+};
+
+export { ProfileModalBody, ProfileModalBodyProps };
