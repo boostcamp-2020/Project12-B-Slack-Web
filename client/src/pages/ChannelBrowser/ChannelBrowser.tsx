5e6379d82e4e841f1b823937c5b9759b10b6ab64
profornnan <profornnan@naver.com>
2020-12-14 11:26:37 +0900

    feat: Browse Page Create Channel Button 동작 구현
    - Create Channel 버튼 클릭시 Create Channel Modal Open
    - 채널 생성 시 생성된 채널로 이동하도록 구현

--- client/src/pages/ChannelBrowser/ChannelBrowser.tsx
+++ client/src/pages/ChannelBrowser/ChannelBrowser.tsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { BrowsePageChannelList, BrowsePageHeader } from '@components/organisms';
 import { BrowsePageControls, BrowsePageSearchBar } from '@components/molecules';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@store/reducers/index';
 import { initChannels } from '@store/actions/channel-action';
+import { createModalOpen } from '@store/actions/modal-action';
 
 interface ChannelBrowserProps {
   children: React.ReactNode;
@@ -21,16 +23,27 @@ const SearchBarWrap = styled.div<any>`
 `;
 
 const ChannelBrowser: React.FC<ChannelBrowserProps> = ({ children, ...props }) => {
+  const history = useHistory();
+
   const dispatch = useDispatch();
   const { channelCount, channels } = useSelector((store: RootState) => store.channel);
+  const { selectedChatroomId } = useSelector((store: RootState) => store.chatroom);
+
+  const handlingCreateButtonClick = () => {
+    dispatch(createModalOpen());
+  };
 
   useEffect(() => {
     dispatch(initChannels());
   }, []);
 
+  useEffect(() => {
+    if (selectedChatroomId) history.push(`/client/${selectedChatroomId}`);
+  }, [selectedChatroomId]);
+
   return (
     <ChannelBrowserContainer {...props}>
-      <BrowsePageHeader />
+      <BrowsePageHeader onClick={handlingCreateButtonClick} />
       <SearchBarWrap>
         <BrowsePageSearchBar />
       </SearchBarWrap>
