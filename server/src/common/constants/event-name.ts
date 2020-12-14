b0574b5d5d0cc8b723082e9bb87e3a46b536e8f9
Kang Dong Hoon <ypd01018@naver.com>
2020-12-15 01:21:51 +0900

    feat: event constant 추가

--- server/src/common/constants/event-name.ts
+++ server/src/common/constants/event-name.ts
@@ -3,11 +3,13 @@ const enum eventName {
   UPDATE_MESSAGE = 'update message',
   DELETE_MESSAGE = 'delete message',
   CREATE_REPLY = 'create reply',
-  UPDATE_REPLY = 'update reply',
+  UPDATE_REPLY = 'upda;te reply',
   DELETE_REPLY = 'delete reply',
   JOIN_CHATROOM = 'join chatroom',
   CREATE_CHATROOM = 'create chatroom',
   CREATE_REACTION = 'create reaction',
-  DELETE_REACTION = 'delete reaction'
+  DELETE_REACTION = 'delete reaction',
+  DISCONNECT = 'disconnect',
+  JOIN_DM = 'join DM'
 }
 export default eventName;
