3398a9418908246b8120c70c1db21c7c975f5b9c
Kang Dong Hoon <ypd01018@naver.com>
2020-12-14 22:34:00 +0900

    feat: message reaction delete socket 구현

--- server/src/socket/handler/message-reaction-handler.ts
+++ server/src/socket/handler/message-reaction-handler.ts
@@ -10,6 +10,16 @@ const messageHandler = {
     const newMessageReaction = await MessageReactionService.getInstance().createMessageReaction(Number(userId), Number(messageId), title, emoji);
     const { chatroomId } = (await MessageServie.getInstance().getMessage(messageId)).chatroom;
     io.to(String(chatroomId)).emit(eventName.CREATE_REACTION, { ...newMessageReaction, chatroomId });
+  },
+
+  async deleteMessageReaction(io, socket, messageReaction) {
+    const req = socket.request;
+    const { userId } = req.user;
+    const { messageId, reactionId } = messageReaction;
+    await MessageReactionService.getInstance().deleteMessageReaction(Number(userId), Number(messageId), Number(reactionId));
+    const newMessageReaction = await MessageReactionService.getInstance().getMessageReaction(Number(messageId), Number(reactionId));
+    const { chatroomId } = (await MessageServie.getInstance().getMessage(messageId)).chatroom;
+    io.to(String(chatroomId)).emit(eventName.DELETE_REACTION, { ...newMessageReaction, chatroomId });
   }
 };
 
