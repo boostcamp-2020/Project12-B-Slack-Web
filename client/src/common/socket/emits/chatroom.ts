c1ca7a92265d0604d040a4f5a3a1d31cb1b348a3
profornnan <profornnan@naver.com>
2020-12-15 16:43:38 +0900

    feat: join DM type 및 함수 정의 (socket)

--- client/src/common/socket/emits/chatroom.ts
+++ client/src/common/socket/emits/chatroom.ts
@@ -1,6 +1,10 @@
-import { JOIN_CHATROOM } from '@socket/types/chatroom-types';
+import { JOIN_CHATROOM, JOIN_DM } from '@socket/types/chatroom-types';
 import socket from '../socketIO';
 
 export const joinChatroom = (chatroomId: number) => {
   socket.emit(JOIN_CHATROOM, { chatroomId });
+};
+
+export const joinDM = (userId: number, chatroomId: number) => {
+  socket.emit(JOIN_DM, { userId, chatroomId });
 };
--- client/src/common/socket/types/chatroom-types.ts
+++ client/src/common/socket/types/chatroom-types.ts
@@ -1,1 +1,2 @@
 export const JOIN_CHATROOM = 'join chatroom';
+export const JOIN_DM = 'join DM';
