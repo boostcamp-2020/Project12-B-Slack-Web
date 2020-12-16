f3648b81b6f912ac627c30a555975ebc61bc9854
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-17 00:40:38 +0900

    feat: Emoji 받아오는 방식 수정
    - react-emoji-render 삭제
    - text를 통한 emoji 생성

--- client/src/components/molecules/EmojiBox/EmojiBox.tsx
+++ client/src/components/molecules/EmojiBox/EmojiBox.tsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Emoji } from '@components/atoms';
 import { color } from '@theme/index';
+import { useDispatch } from 'react-redux';
+import { createMessageReaction, deleteMessageReaction } from '@socket/emits/reaction';
 
 interface EmojiBoxProps {
-  text: string;
-  active: boolean;
+  emoji: string;
+  active?: boolean;
   number: number;
+  messageId: number;
+  reactionId: number;
+  title: string;
 }
 
 const EmojiBoxContainer = styled.div<any>`
@@ -18,6 +23,7 @@ const EmojiBoxContainer = styled.div<any>`
   background-color: ${(props) => (props.isActive ? color.emoji_bg : color.quaternary)};
   border-radius: 1rem;
   cursor: pointer;
+  margin-right: 0.3rem;
   box-shadow: 0 0 0 1px inset ${(props) => (props.isActive ? color.border_tertiary : color.quaternary)};
 
   ${(props) =>
@@ -33,16 +39,21 @@ const EmojiBoxText = styled.p<any>`
   margin: 0;
 `;
 
-const EmojiBox: React.FC<EmojiBoxProps> = ({ active = false, number, text, ...props }) => {
+const EmojiBox: React.FC<EmojiBoxProps> = ({ messageId, reactionId, title, active = false, number, emoji, ...props }) => {
   const [isActive, setActive] = useState(active);
-
+  const dispatch = useDispatch();
   const handlingClick = () => {
+    if (isActive) {
+      deleteMessageReaction({ messageId, reactionId });
+    } else {
+      createMessageReaction({ messageId, title, emoji });
+    }
     setActive(!isActive);
   };
 
   return (
     <EmojiBoxContainer isActive={isActive} onClick={handlingClick} {...props}>
-      <Emoji text={text} />
+      <Emoji text={emoji} />
       <EmojiBoxText> {number} </EmojiBoxText>
     </EmojiBoxContainer>
   );
