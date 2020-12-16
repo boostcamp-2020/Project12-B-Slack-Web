f50d30d32d410e85b71b652e218a1ca410e7da32
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-17 00:37:48 +0900

    feat: EmojiPicker 모달 구현

--- client/src/components/molecules/Actionbar/Actionbar.tsx
+++ client/src/components/molecules/Actionbar/Actionbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { HoverIcon } from '@components/molecules';
+import { HoverIcon, EmojiPicker } from '@components/molecules';
 import { color } from '@theme/index';
 import EmojiIcon from '@imgs/emoji-icon.png';
 import ThreadIcon from '@imgs/thread-icon.png';
@@ -8,6 +8,7 @@ import OptionIcon from '@imgs/option-icon.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { loadThread } from '@store/actions/thread-action';
+import { emojiPickerOpen } from '@store/actions/modal-action';
 
 interface ActionbarProps {
   messageId: number;
@@ -27,13 +28,19 @@ const Actionbar: React.FC<ActionbarProps> = ({ messageId, ...props }) => {
   const history = useHistory();
   const dispatch = useDispatch();
   const chatroomId = useSelector((state: any) => state.chatroom.selectedChatroomId);
+
   const openThread = () => {
     dispatch(loadThread(messageId));
     history.push(`/client/${chatroomId}/thread/${messageId}`);
   };
+  const openEmojiPicker = (e: any) => {
+    const x = window.pageXOffset + e.target.getBoundingClientRect().left;
+    const y = window.pageYOffset + e.target.getBoundingClientRect().top;
+    dispatch(emojiPickerOpen({ x, y, messageId }));
+  };
   return (
     <ActionbarContainer {...props}>
-      <HoverIcon size="medium" src={EmojiIcon} />
+      <HoverIcon size="medium" onClick={openEmojiPicker} src={EmojiIcon} />
       <HoverIcon size="medium" onClick={openThread} src={ThreadIcon} />
       <HoverIcon size="medium" src={OptionIcon} />
     </ActionbarContainer>
