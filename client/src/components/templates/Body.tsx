9345877b610c5e112e8f9ba6742e8dd409fa63c9
Do-ho <hong7511059@koreatech.ac.kr>
2020-12-17 00:42:31 +0900

    feat: socket 정의 및 이벤트 등록

--- client/src/components/templates/Body.tsx
+++ client/src/components/templates/Body.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { channelModalClose, profileModalClose } from '@store/actions/modal-action';
+import { channelModalClose, emojiPickerClose, profileModalClose } from '@store/actions/modal-action';
 import { useDispatch } from 'react-redux';
-import { insertMessage, updateThread } from '@store/actions/chatroom-action';
+import { insertMessage, updateThread, createMessageReaction, deleteMessageReaction } from '@store/actions/chatroom-action';
 import socket from '@socket/socketIO';
 import { CREATE_MESSAGE } from '@socket/types/message-types';
 import { CREATE_REPLY } from '@socket/types/thread-types';
 import { InsertReply } from '@store/actions/thread-action';
+import { socketMessageReactionState, CREATE_MESSAGE_REACTION, DELETE_MESSAGE_REACTION } from '@socket/types/reaction-types';
 
 const StyledBody = styled.div`
   position: relative;
@@ -24,10 +25,17 @@ const Body: React.FC<any> = ({ children }) => {
       dispatch(InsertReply(reply));
       dispatch(updateThread({ messageId: reply.messageId, profileUri: reply.user.profileUri }));
     });
+    socket.on(CREATE_MESSAGE_REACTION, (reaction: socketMessageReactionState) => {
+      dispatch(createMessageReaction(reaction));
+    });
+    socket.on(DELETE_MESSAGE_REACTION, (reaction: any) => {
+      dispatch(deleteMessageReaction(reaction));
+    });
   }, []);
   const handlingLeave = () => {
     dispatch(channelModalClose());
     dispatch(profileModalClose());
+    dispatch(emojiPickerClose());
   };
   return <StyledBody onMouseLeave={handlingLeave}>{children}</StyledBody>;
 };
