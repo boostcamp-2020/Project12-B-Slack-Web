40e60635b4659114b80606a04482cb7e8a95b497
profornnan <profornnan@naver.com>
2020-12-16 18:26:14 +0900

    feat: Channel Store에 Leave Channel 추가
    - Leave Channel types, action, reducer 추가

--- client/src/common/store/reducers/channel-reducer.ts
+++ client/src/common/store/reducers/channel-reducer.ts
@@ -1,4 +1,4 @@
-import { channelState, channelsState, ChannelTypes, INIT_CHANNELS, LOAD_NEXT_CHANNELS, JOIN_CHANNEL } from '../types/channel-types';
+import { channelState, channelsState, ChannelTypes, INIT_CHANNELS, LOAD_NEXT_CHANNELS, JOIN_CHANNEL, LEAVE_CHANNEL } from '../types/channel-types';
 
 const initialState: channelsState = {
   channelCount: 0,
@@ -7,24 +7,36 @@ const initialState: channelsState = {
 
 export default function channelReducer(state = initialState, action: ChannelTypes) {
   switch (action.type) {
-    case INIT_CHANNELS:
+    case INIT_CHANNELS: {
       return {
         channelCount: action.payload.channelCount,
         channels: action.payload.channels
       };
-    case LOAD_NEXT_CHANNELS:
+    }
+    case LOAD_NEXT_CHANNELS: {
       return {
         ...state,
         channels: [...state.channels, ...action.payload.channels]
       };
-    case JOIN_CHANNEL:
+    }
+    case JOIN_CHANNEL: {
       const { chatroomId } = action.payload;
       const channels = state.channels.map((channel: channelState) => {
         if (channel.chatroomId === chatroomId) return { ...channel, isJoined: true };
         return channel;
       });
       return { ...state, channels };
-    default:
+    }
+    case LEAVE_CHANNEL: {
+      const { chatroomId } = action.payload;
+      const channels = state.channels.map((channel: channelState) => {
+        if (channel.chatroomId === chatroomId) return { ...channel, isJoined: false };
+        return channel;
+      });
+      return { ...state, channels };
+    }
+    default: {
       return state;
+    }
   }
 }
