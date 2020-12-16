06f39fc9e8715b2453a7aa1560977c117635a221
Kang Dong Hoon <ypd01018@naver.com>
2020-12-16 12:45:38 +0900

    feat: websocket으로 사용할 수 있도록 인증 방법 변경

--- server/src/socket/middleware/jwt.ts
+++ server/src/socket/middleware/jwt.ts
@@ -1,7 +1,12 @@
 import passport from 'passport';
 
 const jwtMiddleware = (io) => {
-  const wrap = (middleware) => (socket, next) => middleware(socket.request, {}, next);
+  const wrap = (middleware) => (socket, next) => {
+    const { request } = socket;
+    const { token } = socket.handshake.query;
+    request.headers.authorization = token;
+    middleware(request, {}, next);
+  };
   io.use(wrap(passport.authenticate('jwt', { session: false })));
 };
 
