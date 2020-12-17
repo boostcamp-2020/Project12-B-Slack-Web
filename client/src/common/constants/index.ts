6c37e822de70d649437cd5d1321663fe9edfda8e
profornnan <profornnan@naver.com>
2020-12-18 01:41:21 +0900

    fix: URL을 통해 본인이 속해있지 않은 채팅방에 접근 가능한 문제 해결

--- client/src/common/constants/index.ts
+++ client/src/common/constants/index.ts
@@ -2,5 +2,6 @@ import { DefaultSectionName } from './default-section-name';
 import { KeyCode } from './key-code';
 import { ScrollEventType } from './scroll-event-type';
 import { ChatType } from './chat-type';
+import { HttpStatusCode } from './http-status-code';
 
-export { DefaultSectionName, KeyCode, ScrollEventType, ChatType };
+export { DefaultSectionName, KeyCode, ScrollEventType, ChatType, HttpStatusCode };
