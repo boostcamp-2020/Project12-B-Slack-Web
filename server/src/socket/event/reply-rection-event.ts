8753ebb11759234148208174321a3c9fdcb68431
Kang Dong Hoon <ypd01018@naver.com>
2020-12-17 21:44:53 +0900

    feat: delete reply reaction socket 구현

--- server/src/socket/event/reply-rection-event.ts
+++ server/src/socket/event/reply-rection-event.ts
@@ -3,6 +3,7 @@ import eventName from '@constants/event-name';
 
 const messageReactionEvent = (io, socket) => {
   socket.on(eventName.CREATE_REPLY_REACTION, (replyReaction) => replyReactionHandler.createReplyReaction(io, socket, replyReaction));
+  socket.on(eventName.DELETE_REPLY_REACTION, (replyReaction) => replyReactionHandler.deleteReplytReaction(io, socket, replyReaction));
 };
 
 export default messageReactionEvent;
--- server/src/socket/handler/reply-reaction-handler.ts
+++ server/src/socket/handler/reply-reaction-handler.ts
@@ -1,4 +1,5 @@
 import ReplyReactionService from '@service/reply-reaction-service';
+import ReplyService from '@service/reply-service';
 import eventName from '@constants/event-name';
 
 const replyHandler = {
@@ -7,7 +8,20 @@ const replyHandler = {
     const { userId } = req.user;
     const { chatroomId, replyId, title, emoji } = messageReaction;
     const newReplyReaction = await ReplyReactionService.getInstance().createReplyReaction(Number(userId), Number(replyId), title, emoji);
-    io.to(String(chatroomId)).emit(eventName.CREATE_REPLY_REACTION, { ...newReplyReaction, chatroomId });
+    const { messageId } = (await ReplyService.getInstance().getReplyInfo(Number(replyId))).message;
+    console.log({ ...newReplyReaction, messageId, chatroomId });
+    io.to(String(chatroomId)).emit(eventName.CREATE_REPLY_REACTION, { ...newReplyReaction, messageId, chatroomId });
+  },
+
+  async deleteReplytReaction(io, socket, replyReaction) {
+    const req = socket.request;
+    const { userId } = req.user;
+    const { replyId, reactionId, chatroomId } = replyReaction;
+    await ReplyReactionService.getInstance().deleteReplyReaction(Number(userId), Number(replyId), Number(reactionId));
+    const newReplyReaction = await ReplyReactionService.getInstance().getReplyReaction(Number(replyId), Number(reactionId));
+    const { messageId } = (await ReplyService.getInstance().getReplyInfo(Number(replyId))).message;
+    console.log({ ...newReplyReaction, messageId, chatroomId });
+    io.to(String(chatroomId)).emit(eventName.DELETE_REPLY_REACTION, { ...newReplyReaction, messageId, chatroomId });
   }
 };
 
